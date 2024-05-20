bf0b8552f77fce95d877d8d5bb752ca0b05fc028
jeongdari <dannyjeong.au@gmail.com>
2024-05-21 03:54:16 +1000

    Expo Router Screen added

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,18 +1,27 @@
-import { StyleSheet, Image, Text, View } from 'react-native';
-import Colors from '../constants/Colors';
+import { StyleSheet, Image, Text, View, Pressable } from 'react-native';
+import Colors from '@/src/constants/Colors';
+import { Product } from '@/src/types';
+import { Link } from 'expo-router';
 
-const ProductListItem = ({ product }) => {
-  return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: product.image }}
-        style={styles.image}
-        resizeMode="contain"
-      />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
-    </View>
-  );
+export const defaultImage = 'https://stackoverflow.com/does-not-exist.png';
+
+type ProductListItemProps = {
+    product: Product;
+}
+
+function ProductListItem({ product }: ProductListItemProps) {
+    return (
+        <Link href={`/menu/${product.id}`} asChild>
+        <Pressable style={styles.container}>
+            <Image
+                source={{ uri: product.image || defaultImage}}
+                style={styles.image}
+                resizeMode="contain" />
+            <Text style={styles.title}>{product.name}</Text>
+            <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+        </Pressable>
+        </Link>
+    );
 }
 
 export default ProductListItem;
@@ -23,6 +32,8 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     padding: 10,
     overflow: 'hidden',
+    flex: 1,
+    maxWidth: '50%',
   },
   image: {
     width: '100%',
